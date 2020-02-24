df51c8f270c8072b4cde341ed703592845b9a3cd
Cameron Rosier <cameron.rosier@ryerson.ca>
2020-02-23 22:55:36 -0500

    last try for the night at cors..

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -13,7 +13,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
 
-    CORS(app, resources={r'/*': {'origins': '*'}})
+    cors = CORS(app, resources={r"*": {'origins': '*'}})
 
     db.init_app(app)
     flask_bcrypt.init_app(app)
