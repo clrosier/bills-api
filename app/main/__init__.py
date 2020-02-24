0ab3c50aa231e96a9e6c7b31d04825374c1e4461
Cameron Rosier <cameron.rosier@ryerson.ca>
2020-02-24 18:50:29 -0500

    Trying more cors today

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -13,8 +13,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
 
-    cors = CORS(app, resources={r"*": {'origins': '*'}})
-
+    CORS(app)
     db.init_app(app)
     flask_bcrypt.init_app(app)
 
