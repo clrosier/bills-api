ac5ad21fec71daeb6bd449a41925e482cc4ff8b1
Cameron Rosier <cameron.rosier@ryerson.ca>
2020-02-23 22:26:12 -0500

    added cors to api

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -11,6 +12,9 @@ flask_bcrypt = Bcrypt()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
+
+    CORS(app, resources={r'/*': {'origins': '*'}})
+
     db.init_app(app)
     flask_bcrypt.init_app(app)
 
